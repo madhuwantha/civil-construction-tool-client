919279c02bbdbc09d7c022119cab2d1bb8bb9f30
madhuwantha <academicformenew@gmail.com>
2020-10-17 13:13:39 +0530

    remove Redundant alt attribute

--- src/components/MainImageButton.js
+++ src/components/MainImageButton.js
@@ -5,7 +5,7 @@ class MainImageButton extends Component {
   render() {
     return (
       <div className={this.props.class}>
-          <img onClick={this.props?.click} className="main-image-card" width="100%" src="https://source.unsplash.com/user/erondu/700x400" alt="Card image cap" />
+          <img onClick={this.props?.click} className="main-image-card" width="100%" src="https://source.unsplash.com/user/erondu/700x400"  />
       </div>
     );
   }
