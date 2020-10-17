caa4a556a92cf188aa7ebe30ed94eb92aebccdc3
madhuwantha <academicformenew@gmail.com>
2020-10-17 13:17:11 +0530

    img elements must have an alt prop

--- src/components/MainImageButton.js
+++ src/components/MainImageButton.js
@@ -5,7 +5,12 @@ class MainImageButton extends Component {
   render() {
     return (
       <div className={this.props.class}>
-          <img onClick={this.props?.click} className="main-image-card" width="100%" src="https://source.unsplash.com/user/erondu/700x400"  />
+          <img onClick={this.props?.click}
+               className="main-image-card" 
+               width="100%"
+               src="https://source.unsplash.com/user/erondu/700x400"
+               alt="Card image cap"
+          />
       </div>
     );
   }
